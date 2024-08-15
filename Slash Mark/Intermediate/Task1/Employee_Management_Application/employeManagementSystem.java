0d01541dcdfe74ad2a55c9f50fb5e662a61319fd
Manish Giri <142075284+abstracted-m7@users.noreply.github.com>
2024-08-15 21:30:57 +0530

    Slash Mark/Intermediate directory

--- Slash Mark/Intermediate/Task1/Employee_Management_Application/employeManagementSystem.java
+++
@@ -1,6 +0,0 @@
-package Intermediate.Task1.Employee_Management_Application;
-
-
-public class employeManagementSystem {
-    
-}
--- Slash Mark/Intermediate/Task2/Web_Medical_Management_System/main.java
+++
@@ -1,5 +0,0 @@
-package Intermediate.Task2.Web_Medical_Management_System;
-
-public class main {
-    
-}
